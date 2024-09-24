94830a49749388f9c452bc5d7b2fcb1306743066
thomas_TableSurfer <tshort00011@gmail.com>
2024-09-24 12:38:07 -0500

    Modified player.gd to include variable jumps. Still needs Double-Jump protection.

--- spooky_runner_player.gd
+++ spooky_runner_player.gd
@@ -1,11 +1,13 @@
 extends CharacterBody2D
 
-@export var jump_velocity = -300.0
+@export var jump_velocity:float = 0.0
 @export var max_character_gravity = 1550.0
 @export var character_gravity = 900.0
 @export var life = 3
+@export var jumpCount:int = 0
 
 var current_character_gravity = 0.0
+var jumpTimer:float = 0.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -13,13 +15,28 @@ func _ready() -> void:
 
 func _physics_process(delta):
 		# Add the gravity.
+# If jump is just pressed: 
+	if (Input.is_action_just_pressed("Jump") && is_on_floor()):
+		jump_velocity -= 600
+		velocity.y = jump_velocity
+# If jump is pressed/held:
+	if (Input.is_action_pressed("Jump") && jumpTimer < 0.25):
+		
 	if Input.is_action_just_pressed("Jump"):
 		if is_on_floor():
 			velocity.y = jump_velocity
-	if not is_on_floor():
-		if current_character_gravity < max_character_gravity:
-			current_character_gravity += character_gravity * delta
-			velocity.y += current_character_gravity * delta
-	else:
-		current_character_gravity = 0.0
+			jumpTimer += delta 
+# Reset jump
+	if (!Input.is_action_pressed("Jump") && is_on_floor()):
+		jumpTimer = 0.0
+		jump_velocity = 0.0
+# Something to do with gravity
+	if(!is_on_floor()):
+		velocity.y += 40
+	#if not is_on_floor():
+		#if current_character_gravity < max_character_gravity:
+			#current_character_gravity += character_gravity * delta
+			#velocity.y += current_character_gravity * delta
+	#else:
+		#current_character_gravity = 0.0
 	move_and_slide()
