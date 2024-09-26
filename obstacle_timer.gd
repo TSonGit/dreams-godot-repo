daa7333a76d9a5f3255a81de0fbf389b655b47ec
bilbospraggins <smspraggi@gmail.com>
2024-09-26 14:15:55 -0500

    Modified minimum and maximum spawn times.

--- obstacle_timer.gd
+++ obstacle_timer.gd
@@ -1,6 +1,6 @@
 extends Timer
 @export var minObstacleSpawnTime = 3.0
-@export var maxObstacleSpawnTime = 7.0
+@export var maxObstacleSpawnTime = 5.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
