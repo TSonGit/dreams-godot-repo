b479c2707188450881a87192636669b0d114ce46
bilbospraggins <smspraggi@gmail.com>
2024-10-02 18:08:01 -0500

    Added prototype version of 'blocks' of spawning obstacles.

--- spooky_runner_logic_manager.gd
+++ spooky_runner_logic_manager.gd
@@ -9,11 +9,25 @@ func _ready():
 	$monster.position = monster_spawn_location
 
 func _on_obstacle_timer_timeout():
-	_create_obstacle()
+	var obstacle_set = randi_range(1,2)
+	_create_obstacle(obstacle_set)
 	$ObstacleTimer.start()
 
-func _create_obstacle():
-	var obstacle = obstacle_scene.instantiate()
-	add_child(obstacle)
-	var obstacle_spawn_location = $ObjectSpawn.position
-	obstacle.position = obstacle_spawn_location
+func _create_obstacle(set_number):
+	if set_number == 1:
+		var obstacle = obstacle_scene.instantiate()
+		var spawn_position = $ObjectSpawn.position
+		obstacle.position = spawn_position
+		add_child(obstacle)
+	if set_number == 2:
+		var first_obstacle = obstacle_scene.instantiate()
+		var first_spawn_position = $ObjectSpawn.position
+		first_obstacle.position = first_spawn_position
+		
+		var second_obstacle = obstacle_scene.instantiate()
+		var second_spawn_position = Vector2(first_spawn_position.x-50,first_spawn_position.y)
+		second_obstacle.position = second_spawn_position
+		
+		add_child(first_obstacle)
+		add_child(second_obstacle)
+	print(set_number)
